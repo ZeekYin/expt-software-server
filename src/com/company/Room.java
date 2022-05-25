62659ad5a407a54828f730826a0179a2cc9c25e1
ZeekYin <44542418+ZeekYin@users.noreply.github.com>
2022-05-25 14:42:28 +0900

    -

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,28 +1,28 @@
 package com.company;
 
-import java.net.Socket;
 import java.util.*;
 
-public class Room{
+public class Room {
 
     String name;
     String port;
     int liver;
     List<Client> listeners = new ArrayList<Client>();
 
-    public Room(int uid, Client client,String roomname, String port) {
+    public Room(int uid, Client client, String roomname, String port) {
         listeners.add(client);
         name = roomname;
         this.port = port;
         liver = uid;
-        new Thread(()->{
-            while(true){
+        new Thread(() -> {
+            while (this != null) {
                 this.checkConnection();
             }
         }).run();
     }
-    public void checkConnection(){
-        for(Client client : listeners){
+
+    public void checkConnection() {
+        for (Client client : listeners) {
             if (client.socket.getRemoteSocketAddress() == null)
                 listeners.remove(client);
         }
